35b071c8e8fcb7044335b43e6ffb98eca8047ab7
Peter <chow_pete@yahoo.com>
2020-10-28 20:56:35 -0700

    Added intro-comment to tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,13 @@
+#!/bin/python3
+# Filename: tictactoe.py
+# Course: Full Stack Developer Evening Bootcamps
+# Author: Leon || Cleon || Peter, Students
+# Assignment: Tic-Tac-Toe - Version 1
+# Date: 10/28/2020
+# Version 1.0
+
+
+
 # PDX Fullstack: Tic Tac Toe 
 # You will write a **Player** class and **Game** class to model Tic Tac Toe, and a function **main** that models gameplay taking in user inputs through REPL.
 
