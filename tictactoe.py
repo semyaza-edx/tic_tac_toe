782508127d6854f6564e3f794920900579fc1ebb
Leon Siennick <thunderbolt_11@centurylink.net>
2020-10-28 22:01:45 -0600

    created Branch Leon

--- tictactoe.py
+++ tictactoe.py
@@ -14,14 +14,14 @@
 # The Player class has the following properties: 
 #* **name** = *player name*
 #* **token** = *'X' or 'O'*
-# class Player:
+# class Player: <Peter>
 
 
 # The Game class has the following properties:
-# * **board** = *your representation of the board*
+# * **board** = *your representation of the board* <Cleon>
 # You can represent the board however you like, such as a 2D list, tuples, or dictionary.
 # The Game class has the following methods:
-# * `__repr__()` Returns a pretty string representation of the game board
+# <Leon> * `__repr__()` Returns a pretty string representation of the game board
 # * 'move(x, y, player)' place a player's token character at a given coordinate 
 # * 'calc_winner()' determines if a victory condition is met by a Player
 # * 'is_full()' returns True if the game board is full and no victory condition met
@@ -33,3 +33,4 @@
 # main()
 
 print("hello, world!")
+print("Leon's portion of the game.")
