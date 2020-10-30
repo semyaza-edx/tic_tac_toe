84252899dd9b8e0f4e6a1bb353019aaa86471a82
Leon Siennick <thunderbolt_11@centurylink.net>
2020-10-29 21:37:01 -0600

    updated with boad prototype

--- tictactoe.py
+++ tictactoe.py
@@ -16,8 +16,10 @@ import pprint
 # The Player class has the following properties: 
 #* **name** = *player name*
 #* **token** = *'X' or 'O'*
-# class Player: <Peter>
-
+class Player: #<Peter>
+    def __Iinit_(self, name, token):
+        self.name = name
+        self-token = token
 
 # The Game class has the following properties:
 # * **board** = *your representation of the board* <Cleon>
@@ -28,18 +30,55 @@ import pprint
 # * 'calc_winner()' determines if a victory condition is met by a Player
 # * 'is_full()' returns True if the game board is full and no victory condition met
 # * 'is_game_over()' returns True if game board is full or a player has won
-# class Game:
+class Game:
     def __init__(self, board):
-        self.board = board
+        self.board = {'top-L': ' ',
+                        'top-M': ' ',
+                        'top-R': ' ',
+                        'mid-L': ' ',
+                        'mid-M': ' ',
+                        'mid-R': ' ',
+                        'low-L': ' ',
+                        'low-M': ' ',
+                        'low-R': ' '}
     def __repr__():
         # pretty print board
-        pprint(self.board)
-    def move(x, y, player):
+        print(str(self.board['top-L']) + '|' + str(self.board['top-M']) + '|' + str(self.board['top-R']))
+        print('-+-+-')
+        print(str(self.board['mid-L']) + '|' + str(self.board['mid-M']) + '|' + str(self.board['mid-R']))
+        print('-+-+-')
+        print(str(self.board['low-L']) + '|' + str(self.board['low-M']) + '|' + str(self.board['low-R']))
+    def move(turn, player):
         # player's token overwrites data for space in board
+        # board(x, y) = player #either 'X' or 'O'
+        
+    def calc_winner():
+        # check if winning condition met
+        # horizontal_win_conditions = iterate 3 rows
+        # vertical win condition = iterate 3 columns
+        # l-diagonal
+        # r-diagonal
+    def is_full():
+        # check if draw condition met
+    def is_game_over():
+        # check if either calc_wnner OR is_full are True
 
-# def main:
+def main:
+    board = Game()
 
-# main()
+main()
 
 print("hello, world!")
 print("Leon's portion of the game.")
+
+game_on = Game()
+
+
+print(game.board)
+
+player_1 = Player('P1', 'X')
+player_2 = Player('P2', 'O')
+
+print(game_on.move('top-L', 'P1'))
+
+print(game.board)
